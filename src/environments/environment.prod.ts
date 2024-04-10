ecbe98b228f2dd14118138839b238d05f6b9c221
Pradnya Valsangkar <pradnya.61082247@ltimindtree.com>
2024-04-10 11:11:18 +0530

    Removed TEST and PROD env from CD

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { environment as baseEnv } from "./environment";
-
-export const environment = {
-
-    production: true,
-    baseUrl: "https://apps.api.it.philips.com/common/demand-intake",
-};
--- src/environments/environment.test.ts
+++
@@ -1,6 +0,0 @@
-import { environment as baseEnv } from "./environment";
-
-export const environment = {
-    production: false,
-    baseUrl: 'https://dev.apps.api.it.philips.com/test/common/demand-intake',
-};
