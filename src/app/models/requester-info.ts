30e3dfa22e892343ec78ffc8e1b10bda57c835be
Pradnya Valsangkar <pradnya.61082247@ltimindtree.com>
2024-05-17 14:39:37 +0530

    Added requster new fields

--- src/app/models/requester-info.ts
+++ src/app/models/requester-info.ts
@@ -1,32 +1,34 @@
 export class RequesterInfo {
-    program: string = '';
-    domain: string = '';
-    requestedDate: Date = new Date();
-    businessUnit: string = '';
-    market: string = '';
-    cpAPI: boolean = false;
-    cpReport: boolean = false;
-    cpTransactionPlatform: boolean = false;
-    approvedBudget: boolean = false;
-		clarityProjectId: string = "";
-    spoc = [{
-        email: '',
-        role: ''
-      },
-      {
-        email: '',
-        role: ''
-      },
-      {
-        email: '',
-        role: ''
-      },
-      {
-        email: '',
-        role: ''
-      },
-      {
-        email: '',
-        role: ''
-      }];
+  requestedBy: string = '';
+  requesterRole: string = '';
+  program: string = '';
+  domain: string = '';
+  requestedDate: Date = new Date();
+  businessUnit: string[] = [];
+  market: string[] = [];
+  cpAPI: boolean = false;
+  cpReport: boolean = false;
+  cpTransactionPlatform: boolean = false;
+  approvedBudget: boolean = false;
+  clarityProjectId: string = "";
+  spoc = [{
+    email: '',
+    role: ''
+  },
+  {
+    email: '',
+    role: ''
+  },
+  {
+    email: '',
+    role: ''
+  },
+  {
+    email: '',
+    role: ''
+  },
+  {
+    email: '',
+    role: ''
+  }];
 }
