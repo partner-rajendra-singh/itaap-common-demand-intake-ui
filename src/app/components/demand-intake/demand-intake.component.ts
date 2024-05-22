12d0cd2fed836c3dfd9e74b412173aa743972045
Pradnya Valsangkar <pradnya.61082247@ltimindtree.com>
2024-05-22 17:48:34 +0530

    Updated DM

--- src/app/components/demand-intake/demand-intake.component.ts
+++ src/app/components/demand-intake/demand-intake.component.ts
@@ -122,7 +122,7 @@ export class DemandIntakeComponent implements OnInit {
                     visible: (!this.eventService.isMyDemand && this.authService.isCCB())
                         || (this.authService.isRequester() && this.eventService.isMyDemand &&
                             (this.demandIntakeService.demandInformation.introduction.status == 'CCB_HOLD' || this.demandIntakeService.demandInformation.introduction.status == 'ACCEPTED' || this.demandIntakeService.demandInformation.introduction.status == 'REJECTED'))
-                            ||(!this.eventService.isMyDemand && this.authService.isDM() && this.demandIntakeService.demandInformation.introduction.status != 'PENDING_WITH_CCB'),
+                            ||(!this.eventService.isMyDemand && this.authService.isDM() && this.demandIntakeService.demandInformation.introduction.status != 'PENDING_WITH_CCB' && this.demandIntakeService.demandInformation.introduction.status != 'PENDING_WITH_DM'),
                 }
             ];
         }
