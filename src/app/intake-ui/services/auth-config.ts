8952914e822680c1781ddd352e9f0aa118af5974
Rajendra Singh <partner.Rajendra.Singh_1@philips.com>
2024-06-08 15:12:16 +0530

    Update auth-config.ts

--- src/app/intake-ui/services/auth-config.ts
+++ src/app/intake-ui/services/auth-config.ts
@@ -1,11 +1,13 @@
 // src/app/auth-config.ts
 import {BrowserCacheLocation, Configuration, LogLevel} from '@azure/msal-browser';
+import {environment} from "../../../environments/environment";
 
 export const msalConfig: Configuration = {
     auth: {
-        clientId: '7e0686bb-c210-4396-b7f0-2b7d008af0ac', // Replace with your client ID
-        authority: 'https://login.microsoftonline.com/1a407a2d-7675-4d17-8692-b3ac285306e4', // Replace with your tenant ID
-        redirectUri: 'http://localhost:4200', // Replace with your redirect URI
+        clientId: '32020122-420b-4df8-8e1b-137cecd6b9ea', // Replace with your client ID
+        authority: 'https://login.microsoftonline.com/7ec1016c-6ee6-451a-a9dd-82f22f546874', // Replace with your tenant ID
+        // redirectUri: 'http://localhost:4200', // Replace with your redirect URI
+      redirectUri: environment.redirectUri, // Replace with your redirect URI
     },
     cache: {
         cacheLocation: BrowserCacheLocation.LocalStorage, // This configures where your cache will be stored
