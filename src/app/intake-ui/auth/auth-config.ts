168b6f4cfa786577194cf89ff51daf43aedd6219
Rajendra Singh <partner.Rajendra.Singh_1@philips.com>
2024-06-09 18:57:30 +0530

    Update auth-config.ts

--- src/app/intake-ui/auth/auth-config.ts
+++ src/app/intake-ui/auth/auth-config.ts
@@ -4,11 +4,11 @@ import {MsalGuardConfiguration, MsalInterceptorConfiguration} from "@azure/msal-
 
 export const msalConfig: Configuration = {
   auth: {
-    // clientId: '7e0686bb-c210-4396-b7f0-2b7d008af0ac', // Replace with your client ID
-    // authority: 'https://login.microsoftonline.com/1a407a2d-7675-4d17-8692-b3ac285306e4', // Replace with your tenant ID
+    clientId: '7e0686bb-c210-4396-b7f0-2b7d008af0ac', // Replace with your client ID
+    authority: 'https://login.microsoftonline.com/1a407a2d-7675-4d17-8692-b3ac285306e4', // Replace with your tenant ID
     // redirectUri: 'http://localhost:4200', // Replace with your redirect URI
-    clientId: '32020122-420b-4df8-8e1b-137cecd6b9ea', // Replace with your client ID
-    authority: 'https://login.microsoftonline.com/7ec1016c-6ee6-451a-a9dd-82f22f546874', // Replace with your tenant ID
+    // clientId: '32020122-420b-4df8-8e1b-137cecd6b9ea', // Replace with your client ID
+    // authority: 'https://login.microsoftonline.com/7ec1016c-6ee6-451a-a9dd-82f22f546874', // Replace with your tenant ID
     redirectUri: environment.redirectUri, // Replace with your redirect URI
   },
   cache: {
